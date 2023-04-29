c847918c49123c5f9447c89ff1ad2f903673ab59
abdull461 <abdullahabubakar461@gmail.com>
2023-04-29 21:09:19 +0100

    adding form and jinja for data

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from weather import main as get_weather
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    data = None
+    if request.method == 'POST':
+        city = request.form['cityName']
+        state = request.form['stateName']
+        country = request.form['countryName']
+        data = get_weather(city, state, country)
+    return render_template('index.html', data=data)
 
 if __name__=='__main__':
     app.run(debug=True)
