a72033f024faf260731479c2a220a22db761f77c
abdull461 <abdullahabubakar461@gmail.com>
2023-04-29 10:31:33 +0100

    designing the homepage

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from weather import main as get_weather
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
